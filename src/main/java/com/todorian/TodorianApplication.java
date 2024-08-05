f907589643546680ad0dd4af700179bc98a55502
INUK-ai <arheon2469@gmail.com>
2024-08-05 12:09:26 +0900

    Feat: TODORIAN Build.gradle

---
+++ src/main/java/com/todorian/TodorianApplication.java
@@ -0,0 +1,13 @@
+package com.todorian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodorianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodorianApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/todorian/TodorianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todorian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodorianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
