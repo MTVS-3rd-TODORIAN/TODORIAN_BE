18982e658a40fa4f8c44e192fabe43fcc8b1d658
Virtus_Chae <ybd4731@gmail.com>
2024-08-06 17:06:03 +0900

    feat: ItemDto 에서 class 단위의 @Builder 사용 제거

--- src/main/java/com/todorian/item/dto/ItemDto.java
+++ src/main/java/com/todorian/item/dto/ItemDto.java
@@ -1,18 +1,22 @@
 package com.todorian.item.dto;
 
 import com.todorian.item.domain.ItemCategory;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
 public class ItemDto {
     private long itemId;
     private String itemName;
     private String itemDescription;
     private ItemCategory itemCategory;
+
+    @Builder
+    public ItemDto(String itemName, String itemDescription, ItemCategory itemCategory) {
+        this.itemName = itemName;
+        this.itemDescription = itemDescription;
+        this.itemCategory = itemCategory;
+    }
 }
