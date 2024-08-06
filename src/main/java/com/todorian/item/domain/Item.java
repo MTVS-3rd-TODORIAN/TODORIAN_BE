5e126ab6e209173f975676228ee9aa5105f8358d
Virtus_Chae <ybd4731@gmail.com>
2024-08-06 14:55:57 +0900

    feat: Item Entity 수정 - Lombok 추가 - 테이블 이름 tbl_ 로 시작하도록 변경 - BaseTimeEntity 상속하도록 변경

--- src/main/java/com/todorian/item/domain/Item.java
+++ src/main/java/com/todorian/item/domain/Item.java
@@ -1,18 +1,23 @@
 package com.todorian.item.domain;
 
-import jakarta.persistence.Column;
+import com.todorian.BaseTimeEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table(name = "item")
-public class Item {
+@Table(name = "tbl_item")
+public class Item extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "item_id")
     private long itemId;
 }
