b544a1c54611872de0bbcbd116389c39824c9510
seoweed <darkweed1106@gmail.com>
2024-08-06 14:49:01 +0900

    Feat: Todo entity 생성 및 수정

--- src/main/java/com/todorian/Todo.java
+++
@@ -1,5 +0,0 @@
-package com.todorian;
-
-public class Todo {
-
-}
---
+++ src/main/java/com/todorian/todo/Todo.java
@@ -0,0 +1,33 @@
+package com.todorian.todo;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Table(name = "tbl_todo")
+public class Todo {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "TODO_ID")
+    private Long todoId;
+
+    @Column(name = "TODO_CONTENT")
+    private String todoContent;
+
+    @Column(name = "TODO_CREAT_AT")
+    private LocalDate todoCreatedAt;
+
+    @Builder
+    public Todo(String todoContent, LocalDate todoCreatedAt) {
+        this.todoContent = todoContent;
+        this.todoCreatedAt = todoCreatedAt;
+    }
+}
