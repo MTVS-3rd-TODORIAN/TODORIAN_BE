f4b836cf98a45e71cf93517359761310c2e87424
Nuk <nuk@Nukui-MacBookAir.local>
2024-08-12 06:00:07 +0900

    Refactor: MemberRepository extends JPARepository

--- src/main/java/com/todorian/member/repository/MemberRepository.java
+++ src/main/java/com/todorian/member/repository/MemberRepository.java
@@ -1,8 +1,11 @@
 package com.todorian.member.repository;
 
+import com.todorian.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
 
 }
