fdc5883669de89b084aca9905c1c67976e85830a
Nuk <nuk@Nukui-MacBookAir.local>
2024-08-12 05:59:21 +0900

    Feat: MemberRepository

---
+++ src/main/java/com/todorian/member/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.todorian.member.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository {
+
+}
--- src/main/java/com/todorian/member/service/MemberService.java
+++ src/main/java/com/todorian/member/service/MemberService.java
@@ -1,10 +1,21 @@
 package com.todorian.member.service;
 
 import com.todorian.member.dto.MemberCreateRequestDTO;
+import com.todorian.member.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     public void save(MemberCreateRequestDTO memberCreateRequestDTO) {
 
