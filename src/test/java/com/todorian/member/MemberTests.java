8d97e9e52698730851972fa1e829b1ee8456212d
Nuk <nuk@Nukui-MacBookAir.local>
2024-08-12 05:50:38 +0900

    Feat: MemberTests

---
+++ src/test/java/com/todorian/member/MemberTests.java
@@ -0,0 +1,9 @@
+package com.todorian.member;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public class MemberTests {
+}
