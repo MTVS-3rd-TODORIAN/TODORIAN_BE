34daca08c3432eab161c1dccda48db9c5763b92e
Nuk <nuk@Nukui-MacBookAir.local>
2024-08-12 05:54:36 +0900

    Test: createMember

--- src/test/java/com/todorian/member/MemberTests.java
+++ src/test/java/com/todorian/member/MemberTests.java
@@ -1,8 +1,15 @@
 package com.todorian.member;
 
+import com.todorian.member.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Stream;
 
 @SpringBootTest
 @Transactional
@@ -10,4 +17,17 @@ public class MemberTests {
 
     @Autowired
     private MemberService memberService;
+
+    private static Stream<Arguments> createMember() {
+        return Stream.of(
+                Arguments.of("user1@test.com", "test1234!")
+        );
+    }
+
+    @DisplayName("회원 추가 테스트")
+    @ParameterizedTest
+    @MethodSource("createMember")
+    void createMember(String email, String password) {
+
+    }
 }
