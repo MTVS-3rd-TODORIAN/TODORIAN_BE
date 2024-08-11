eedcc239f1232caaf00dc4fa23aa1499dbea7e6f
Nuk <nuk@Nukui-MacBookAir.local>
2024-08-12 05:51:48 +0900

    Feat: MemberService

--- src/test/java/com/todorian/member/MemberTests.java
+++ src/test/java/com/todorian/member/MemberTests.java
@@ -1,9 +1,13 @@
 package com.todorian.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
 public class MemberTests {
+
+    @Autowired
+    private MemberService memberService;
 }
