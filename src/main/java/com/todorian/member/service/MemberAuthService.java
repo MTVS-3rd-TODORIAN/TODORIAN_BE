88b434d047365824149e36cb866cf30151f16b68
Nuk <nuk@Nukui-MacBookAir.local>
2024-08-25 00:32:35 +0900

    Feat: MemberAuthService - login

--- src/main/java/com/todorian/member/service/MemberAuthService.java
+++ src/main/java/com/todorian/member/service/MemberAuthService.java
@@ -1,6 +1,7 @@
 package com.todorian.member.service;
 
 import com.todorian._core.error.exception.Exception400;
+import com.todorian._core.jwt.JWTTokenProvider;
 import com.todorian.member.domain.Authority;
 import com.todorian.member.domain.Member;
 import com.todorian.member.domain.SocialType;
@@ -12,6 +13,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,6 +32,8 @@ public class MemberAuthService {
     private final MemberRepository memberRepository;
 
     private final PasswordEncoder passwordEncoder;
+    private final JWTTokenProvider jwtTokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
     public void save(MemberCreateRequestDTO memberCreateRequestDTO) {
 
@@ -66,7 +73,15 @@ public class MemberAuthService {
     }
 
     public MemberResponseDTO.authTokenDTO login(HttpServletRequest httpServletRequest, MemberRequestDTO.loginDTO requestDTO) {
-        return null;
+
+        // 1. 이메일 확인
+        Member member = memberRepository.findByEmail(requestDTO.email())
+                .orElseThrow(() -> new Exception400("가입 되지 않은 이메일입니다."));
+
+        // 2. 비밀번호 확인
+        checkValidPassword(requestDTO.password(), member.getPassword());
+
+        return getAuthTokenDTO(requestDTO.email(), requestDTO.password(), httpServletRequest);
     }
 
     public MemberResponseDTO.authTokenDTO reissueToken(HttpServletRequest httpServletRequest) {
@@ -106,5 +121,25 @@ public class MemberAuthService {
                 .socialType(SocialType.NONE)
                 .authority(Authority.USER)
                 .build();
+    }
+
+    // 토큰 발급
+    protected MemberResponseDTO.authTokenDTO getAuthTokenDTO(String email, String password, HttpServletRequest httpServletRequest) {
+
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken
+                = new UsernamePasswordAuthenticationToken(email, password);
+        AuthenticationManager manager = authenticationManagerBuilder.getObject();
+        Authentication authentication = manager.authenticate(usernamePasswordAuthenticationToken);
+
+        MemberResponseDTO.authTokenDTO authTokenDTO = jwtTokenProvider.generateToken(authentication);
+
+        refreshTokenRedisRepository.save(RefreshToken.builder()
+                .id(authentication.getName())
+                .authorities(authentication.getAuthorities())
+                .refreshToken(authTokenDTO.refreshToken())
+                .build()
+        );
+
+        return authTokenDTO;
     }
 }
