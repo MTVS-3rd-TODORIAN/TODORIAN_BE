550d863bc750e2eb4aa2043ecb58c466bd0a7f93
INUK-ai <arheon2469@gmail.com>
2024-08-28 15:05:45 +0900

    Test: MemberAuthService - login

--- src/test/java/com/todorian/member/service/MemberAuthServiceTest.java
+++ src/test/java/com/todorian/member/service/MemberAuthServiceTest.java
@@ -1,10 +1,13 @@
 package com.todorian.member.service;
 
 import com.todorian.member.command.application.dto.MemberRequestDTO;
+import com.todorian.member.command.application.dto.MemberResponseDTO;
 import com.todorian.member.command.application.service.MemberAuthService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -14,11 +17,17 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 public class MemberAuthServiceTest {
 
     @Autowired
     private MemberAuthService memberAuthService;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
 
     @BeforeEach
     void setUp() {
@@ -50,8 +59,28 @@ public class MemberAuthServiceTest {
                 userConfirmPassword
         );
 
-        Assertions.assertDoesNotThrow(
+        assertDoesNotThrow(
                 () -> memberAuthService.signUp(requestDTO)
         );
+    }
+
+    @DisplayName("login 테스트")
+    @Test
+    void login() {
+
+        MemberRequestDTO.loginDTO requestDTO = new MemberRequestDTO.loginDTO(
+                "user@test.com",
+                "test1234"
+        );
+
+        // when
+        MemberResponseDTO.authTokenDTO  authTokenDTO = memberAuthService.login(httpServletRequest, requestDTO);
+
+        // then
+        System.out.println("authTokenDTO = " + authTokenDTO);
+
+        assertNotNull(authTokenDTO);
+        assertNotNull(authTokenDTO.accessToken());
+        assertNotNull(authTokenDTO.refreshToken());
     }
 }
