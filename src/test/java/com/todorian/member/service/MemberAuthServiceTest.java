9969ae71699c30c5c819fcfe18cbb4e6b62c4faa
INUK-ai <arheon2469@gmail.com>
2024-09-02 09:07:39 +0900

    Test: MemberAuthService - reissueToken

--- src/test/java/com/todorian/member/service/MemberAuthServiceTest.java
+++ src/test/java/com/todorian/member/service/MemberAuthServiceTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +20,7 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class MemberAuthServiceTest {
@@ -26,19 +28,19 @@ public class MemberAuthServiceTest {
     @Autowired
     private MemberAuthService memberAuthService;
 
-    @Autowired
+    @Mock
     private HttpServletRequest httpServletRequest;
 
     @BeforeEach
     void setUp() {
-        MemberRequestDTO.signUpDTO requestDTO = new MemberRequestDTO.signUpDTO(
+        MemberRequestDTO.signUpDTO signUpDTO = new MemberRequestDTO.signUpDTO(
                 "userNickName",
                 "user@test.com",
                 "test1234",
                 "test1234"
         );
 
-        memberAuthService.signUp(requestDTO);
+        memberAuthService.signUp(signUpDTO);
     }
 
     private static Stream<Arguments> createMember() {
@@ -74,7 +76,7 @@ public class MemberAuthServiceTest {
         );
 
         // when
-        MemberResponseDTO.authTokenDTO  authTokenDTO = memberAuthService.login(httpServletRequest, requestDTO);
+        MemberResponseDTO.authTokenDTO authTokenDTO = memberAuthService.login(httpServletRequest, requestDTO);
 
         // then
         System.out.println("authTokenDTO = " + authTokenDTO);
@@ -82,5 +84,27 @@ public class MemberAuthServiceTest {
         assertNotNull(authTokenDTO);
         assertNotNull(authTokenDTO.accessToken());
         assertNotNull(authTokenDTO.refreshToken());
+    }
+
+    @DisplayName(("토큰 재발급 테스트"))
+    @Test
+    void reissueToken() {
+
+        // given
+        MemberRequestDTO.loginDTO loginDTO = new MemberRequestDTO.loginDTO(
+                "user@test.com",
+                "test1234"
+        );
+
+        // 실제 HTTP 요청에서 토큰 추출
+        MemberResponseDTO.authTokenDTO authTokenDTO = memberAuthService.login(httpServletRequest, loginDTO);
+
+        // when
+        when(httpServletRequest.getHeader("Authorization")).thenReturn("Bearer " + authTokenDTO.refreshToken());
+        MemberResponseDTO.authTokenDTO newAuthTokenDTO = memberAuthService.reissueToken(httpServletRequest);
+
+        // then
+        assertNotNull(newAuthTokenDTO);
+        System.out.println("newAuthTokenDTO = " + newAuthTokenDTO);
     }
 }
