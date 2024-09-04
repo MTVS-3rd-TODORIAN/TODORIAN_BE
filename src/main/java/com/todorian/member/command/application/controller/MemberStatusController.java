9d862922eb2c73899b58fc9018ca91521671d10f
INUK-ai <arheon2469@gmail.com>
2024-09-04 10:06:25 +0900

    Feat: MemberStatusController

---
+++ src/main/java/com/todorian/member/command/application/controller/MemberStatusController.java
@@ -0,0 +1,15 @@
+package com.todorian.member.command.application.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/auth")
+public class MemberStatusController {
+
+    private final MemberStatusService memberStatusService;
+}
