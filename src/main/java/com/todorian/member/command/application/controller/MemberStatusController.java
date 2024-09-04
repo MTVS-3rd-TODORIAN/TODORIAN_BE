922dd3f0316e7307be2a2d5de5004772d18e0429
INUK-ai <arheon2469@gmail.com>
2024-09-04 13:21:36 +0900

    Feat: MemberStatusController - deleteMember

--- src/main/java/com/todorian/member/command/application/controller/MemberStatusController.java
+++ src/main/java/com/todorian/member/command/application/controller/MemberStatusController.java
@@ -1,7 +1,12 @@
 package com.todorian.member.command.application.controller;
 
+import com.todorian._core.utils.ApiUtils;
+import com.todorian.member.command.application.service.MemberStatusService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberStatusController {
 
     private final MemberStatusService memberStatusService;
+
+    /*
+        회원 탈퇴
+     */
+    @DeleteMapping("/{memberId}")
+    public ResponseEntity<?> deleteMember(@PathVariable Long memberId) {
+
+        memberStatusService.deleteMember(memberId);
+
+        return ResponseEntity.ok().body(ApiUtils.success(null));
+    }
+
+
+    /*
+        회원 복구
+     */
 }
