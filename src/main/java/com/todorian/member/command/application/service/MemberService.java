a7739ee0ef103c4e679da0c2efd18623943fe343
INUK-ai <arheon2469@gmail.com>
2024-09-19 17:29:17 +0900

    Feat: MemberService

---
+++ src/main/java/com/todorian/member/command/application/service/MemberService.java
@@ -0,0 +1,19 @@
+package com.todorian.member.command.application.service;
+
+
+import com.todorian.member.command.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+
+}
