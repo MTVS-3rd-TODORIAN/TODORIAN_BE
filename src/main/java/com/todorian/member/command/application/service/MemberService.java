66123067b39e5a21e5312687d414b3f3f62534e6
INUK-ai <arheon2469@gmail.com>
2024-09-19 17:39:29 +0900

    Feat: MemberService - getMemberProfile

--- src/main/java/com/todorian/member/command/application/service/MemberService.java
+++ src/main/java/com/todorian/member/command/application/service/MemberService.java
@@ -1,7 +1,8 @@
 package com.todorian.member.command.application.service;
 
-
+import com.todorian._core.error.exception.Exception401;
 import com.todorian.member.command.application.dto.MemberResponseDTO;
+import com.todorian.member.command.domain.model.Member;
 import com.todorian.member.command.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +19,10 @@ public class MemberService {
 
 
     public MemberResponseDTO.getMemberProfileDTO getMemberProfile(Long currentMemberId) {
-        return null;
+
+        Member member = memberRepository.findById(currentMemberId)
+                .orElseThrow(() -> new Exception401("해당 회원을 찾을 수 없습니다."));
+
+        return new MemberResponseDTO.getMemberProfileDTO(member.getNickName());
     }
 }
