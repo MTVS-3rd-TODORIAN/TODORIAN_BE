1df2a20399f4fb081995980d31a19eae59b8c91a
Virtus_Chae <ybd4731@gmail.com>
2024-10-07 09:32:03 +0900

    feat: 프로필에 따른 로그 출력 설정

--- src/main/java/com/todorian/TodorianApplication.java
+++ src/main/java/com/todorian/TodorianApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -22,13 +23,12 @@ public class TodorianApplication {
         SpringApplication.run(TodorianApplication.class, args);
     }
 
+    @Profile("local") // profile 이 local 일 때만 돌아 감
     @Bean
     CommandLineRunner localServerStart(MemberRepository memberRepository, PasswordEncoder passwordEncoder) {
-        return args -> {
-            memberRepository.saveAll(Arrays.asList(
-                    newMember("test@test.com", "test1234!", passwordEncoder)
-            ));
-        };
+        return args -> memberRepository.saveAll(Arrays.asList(
+                newMember("test@test.com", "test1234!", passwordEncoder)
+        ));
     }
 
     private Member newMember(String email, String password, PasswordEncoder passwordEncoder) {
