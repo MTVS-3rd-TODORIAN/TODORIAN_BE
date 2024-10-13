667513aa44732172755856df8dcdd38b96ff4dca
Virtus_Chae <ybd4731@gmail.com>
2024-10-14 01:16:10 +0900

    feat: 코인 내역 생성 시 BE 에서 memberId 주입되도록 추가

--- src/main/java/com/todorian/coin/application/CoinController.java
+++ src/main/java/com/todorian/coin/application/CoinController.java
@@ -3,6 +3,7 @@ package com.todorian.coin.application;
 import com.todorian._core.utils.SecurityUtils;
 import com.todorian.coin.domain.model.CoinFindResponseDto;
 import com.todorian.coin.domain.model.CoinSaveRequestDto;
+import com.todorian.coin.domain.model.CoinSaveResponseDto;
 import com.todorian.coin.domain.service.CoinService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,9 +27,8 @@ public class CoinController {
     }
 
     @PostMapping
-    ResponseEntity<CoinFindResponseDto> save(CoinSaveRequestDto coinSaveRequestDto) {
-        Long memberId = SecurityUtils.getCurrentMemberId();
-        
+    ResponseEntity<CoinSaveResponseDto> save(@RequestBody CoinSaveRequestDto coinSaveRequestDto) {
+        coinSaveRequestDto.setMemberId(SecurityUtils.getCurrentMemberId());
         return coinService.save(coinSaveRequestDto);
     }
 
