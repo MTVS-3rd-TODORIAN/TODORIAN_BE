c85ad9722f06c9dd25d7efcf88c69db2a268875d
Virtus_Chae <ybd4731@gmail.com>
2024-10-14 01:17:15 +0900

    feat: Lombok 애너테이션 추가

--- src/main/java/com/todorian/coin/domain/model/CoinSaveRequestDto.java
+++ src/main/java/com/todorian/coin/domain/model/CoinSaveRequestDto.java
@@ -1,15 +1,23 @@
 package com.todorian.coin.domain.model;
 
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Builder
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
 public class CoinSaveRequestDto {
+    @Setter
     private Long memberId;
     private LocalDateTime coinDateTime;
     private Long coinAmount;
-    private CoinReason coinReason;
+    private CoinReason coinReason; //     "coinReason": "GAME_WIN",
+
     private Long coinForeignId;
 }
