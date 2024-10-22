c89c2f05e249aca6583872bd1525f108b11fc33b
INUK-ai <arheon2469@gmail.com>
2024-10-22 12:58:36 +0900

    Feat: MemberAdminService - login

--- src/main/java/com/todorian/member/command/application/dto/MemberAdminResponseDTO.java
+++ src/main/java/com/todorian/member/command/application/dto/MemberAdminResponseDTO.java
@@ -3,7 +3,11 @@ package com.todorian.member.command.application.dto;
 public class MemberAdminResponseDTO {
 
     public record loginDTO(
-
+            String grantType,
+            String accessToken,
+            Long accessTokenValidTime,
+            String refreshToken,
+            Long refreshTokenValidTime
     ) {
     }
 }
--- src/main/java/com/todorian/member/command/application/service/MemberAdminService.java
+++ src/main/java/com/todorian/member/command/application/service/MemberAdminService.java
@@ -1,8 +1,15 @@
 package com.todorian.member.command.application.service;
 
+import com.todorian._core.error.exception.Exception400;
+import com.todorian._core.error.exception.Exception403;
+import com.todorian.member.command.application.dto.MemberAdminRequestDTO;
+import com.todorian.member.command.application.dto.MemberAdminResponseDTO;
+import com.todorian.member.command.domain.model.Member;
+import com.todorian.member.command.domain.model.property.Authority;
 import com.todorian.member.command.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,5 +19,41 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class MemberAdminService {
 
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    /*
+        관리자 로그인
+     */
+    public MemberAdminResponseDTO.loginDTO login(MemberAdminRequestDTO.loginDTO requestDTO) {
+
+        // 1. 이메일 확인
+        Member member = memberRepository.findByEmail(requestDTO.email())
+                .orElseThrow(() -> new Exception400("가입 되지 않은 이메일입니다."));
+
+        // 2. 비밀번호 확인
+        checkValidPassword(requestDTO.password(), member.getPassword());
+
+        // 3. 회원 권한 확인
+        checkAdminAuthority(member);
+
+        return null;
+    }
+
+    // 비밀번호 확인
+    private void checkValidPassword(String rawPassword, String encodedPassword) {
+
+        if(!passwordEncoder.matches(rawPassword, encodedPassword)) {
+            throw new Exception400("비밀번호가 유효하지 않습니다.");
+        }
+    }
+
+    // 회원 권한 확인
+    private void checkAdminAuthority(Member member) {
+
+        if(member.getAuthority() != Authority.ADMIN) {
+            throw new Exception403("관리자 권한이 없는 계정입니다.");
+        }
+    }
 }
