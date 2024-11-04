86065a17b1b39b3c304ebde6eb8ba8f6a6df38d6
INUK-ai <arheon2469@gmail.com>
2024-11-04 12:40:29 +0900

    Feat: MemberSystemService

--- src/main/java/com/todorian/system/command/application/controller/MemberSystemController.java
+++ src/main/java/com/todorian/system/command/application/controller/MemberSystemController.java
@@ -1,5 +1,6 @@
 package com.todorian.system.command.application.controller;
 
+import com.todorian.system.command.application.service.MemberSystemService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
