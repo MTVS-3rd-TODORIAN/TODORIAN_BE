1701677e50e9fbd0dde05a609c3fa583f05790ef
INUK-ai <arheon2469@gmail.com>
2024-11-13 13:03:02 +0900

    Feat: AdminCommandResponseDTO - patchTodoPointRatioDTO

--- src/main/java/com/todorian/admin/command/application/dto/AdminCommandResponseDTO.java
+++ src/main/java/com/todorian/admin/command/application/dto/AdminCommandResponseDTO.java
@@ -2,19 +2,8 @@ package com.todorian.admin.command.application.dto;
 
 public class AdminCommandResponseDTO {
 
-    // 토큰 발급
-    public record authTokenDTO(
-            String grantType,
-            String accessToken,
-            Long accessTokenValidTime,
-            String refreshToken,
-            Long refreshTokenValidTime
+    public record patchTodoPointRatioDTO(
+            Integer ratio
     ) {
-    }
-
-    // 회원 프로필
-    public record getMemberProfileDTO(
-            String nickName
-    ){
     }
 }
