cdba4a639695d0b8e100c5b256f493bdbe943eb0
INUK-ai <arheon2469@gmail.com>
2024-11-13 13:06:34 +0900

    Feat: AdminCommandService - patchTodoPointRatio

--- src/main/java/com/todorian/admin/command/application/service/AdminCommandService.java
+++ src/main/java/com/todorian/admin/command/application/service/AdminCommandService.java
@@ -1,5 +1,8 @@
 package com.todorian.admin.command.application.service;
 
+import com.todorian.admin.command.application.dto.AdminCommandRequestDTO;
+import com.todorian.admin.command.application.dto.AdminCommandResponseDTO;
+import com.todorian.todo.point.command.application.service.TodoPointCommandService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,4 +13,18 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Service
 public class AdminCommandService {
+
+    private final TodoPointCommandService todoPointCommandService;
+
+    /*
+        ToddPoint Ratio 변경
+     */
+    public AdminCommandResponseDTO.patchTodoPointRatioDTO patchTodoPointRatio(AdminCommandRequestDTO.patchTodoPointRatioDTO requestDTO) {
+
+        Integer ratio = todoPointCommandService.patchTodoPointRatio(requestDTO.ratio());
+
+        return new AdminCommandResponseDTO.patchTodoPointRatioDTO(
+                ratio
+        );
+    }
 }
