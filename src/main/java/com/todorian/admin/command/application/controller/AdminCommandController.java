d6cf67371353ba880901aa0f7d3deb2c3cdfde7c
INUK-ai <arheon2469@gmail.com>
2024-11-14 10:51:25 +0900

    Feat: AdminCommandController - patchCharacterGrowthCriteria

--- src/main/java/com/todorian/admin/command/application/controller/AdminCommandController.java
+++ src/main/java/com/todorian/admin/command/application/controller/AdminCommandController.java
@@ -7,10 +7,7 @@ import com.todorian.admin.command.application.service.AdminCommandService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -29,5 +26,17 @@ public class AdminCommandController {
         AdminCommandResponseDTO.patchTodoPointRatioDTO responseDTO = adminCommandService.patchTodoPointRatio(requestDTO);
 
         return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
+    }
+
+    /*
+        Character Level 별 growthCriteria 변경
+     */
+    @PutMapping("/characters/{characterId}/growth-criteria")
+    public ResponseEntity<?> patchCharacterGrowthCriteria(@PathVariable("characterId") Long characterId,
+                                                          @RequestBody AdminCommandRequestDTO.patchCharacterGrowthCriteriaDTO requestDTO) {
+
+        adminCommandService.patchCharacterGrowthCriteria(characterId, requestDTO);
+
+        return ResponseEntity.ok().body(ApiUtils.success(null));
     }
 }
