8be1472adcfa6dee7cd7b40debbdb8562b8f2c98
INUK-ai <arheon2469@gmail.com>
2024-11-21 22:04:59 +0900

    Setting: Logging AOP

---
+++ src/main/java/com/todorian/_core/aop/LoggingAspect.java
@@ -0,0 +1,56 @@
+package com.todorian._core.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Pointcut("execution(* com.todorian..controller..*.*(..))")
+    public void pointcut() {}
+
+    @Around("pointcut()")
+    public Object aroundLog(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+
+        // 메소드 정보 추출
+        Method method = getMethod(proceedingJoinPoint);
+
+        log.info("===== method name = {} =====", method.getName());
+
+        // 파라미터 추출
+        Object[] args = proceedingJoinPoint.getArgs();
+
+        if (args.length == 0) {
+            log.info("No Parameter");
+        }
+
+        for (Object arg : args) {
+            log.info("Parameter Type = {}", arg.getClass().getSimpleName());
+            log.info("Parameter Value = {}", arg);
+        }
+
+        // 실제 메소드 실행
+        Object response = proceedingJoinPoint.proceed(args);
+
+        log.info("Response Type = {}", response.getClass().getSimpleName());
+        log.info("Response Value = {}", response);
+
+        return response;
+    }
+
+    // Method 정보 추출
+    private Method getMethod(ProceedingJoinPoint proceedingJoinPoint) {
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+        return methodSignature.getMethod();
+    }
+}
